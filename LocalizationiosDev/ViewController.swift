d85c872527f2c95b7f0960805d5a132dc353490d
TusharAnawadiya <tusharanawadiya444@gmail.com>
2023-12-22 17:11:19 +0530

    Xcode Project Update

--- LocalizationiosDev/ViewController.swift
+++ LocalizationiosDev/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var seg: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+  
+    
+    // Segment Controller -----------------
+    @IBAction func selectAtion(_ sender: Any) {
+        if seg.selectedSegmentIndex == 0 {
+            var path = Bundle.main.path(forResource:"en", ofType: "lproj")
+            var bundle = Bundle(path: path!)
+            label1.text = NSLocalizedString("label1Key",bundle: bundle!, comment: "")
+            label2.text = NSLocalizedString("label2Key",bundle: bundle!, comment: "")
+        }
+        else {
+            var path = Bundle.main.path(forResource:"es", ofType: "lproj")
+            var bundle = Bundle(path: path!)
+            label1.text = NSLocalizedString("label1Key",bundle: bundle!, comment: "")
+            label2.text = NSLocalizedString("label2Key",bundle: bundle!, comment: "")
+        }
+    }
+    
+    
+    // Select Action Button ------------------- 
+    @IBAction func selectAction(_ sender: Any) {
+                let alert:UIAlertController = UIAlertController(title:"Select language", message: "", preferredStyle: UIAlertController.Style.actionSheet)
+        
+                let En:UIAlertAction = UIAlertAction(title: "English", style: .default, handler: {_ in
+                   
+                        var path = Bundle.main.path(forResource: "en", ofType: "lproj")
+                        var bundle = Bundle(path: path!)
+                        self.label1.text = NSLocalizedString("label1Key", bundle: bundle!, comment: "")
+                        self.label2.text = NSLocalizedString("label2Key",bundle: bundle!, comment: "")
+        
+                   
+                    })
+                
+                let Spanish:UIAlertAction = UIAlertAction(title: "Spanish", style: .default){_ in
+                    var path = Bundle.main.path(forResource: "es", ofType: "lproj")
+                    var bundle = Bundle(path: path!)
+                    self.label1.text = NSLocalizedString("label1Key", bundle: bundle!, comment: "")
+                    self.label2.text = NSLocalizedString("label2Key",bundle: bundle!, comment: "")
+                }
+                alert.addAction(En)
+                alert.addAction(Spanish)
+                self.present(alert, animated: true)
+    }
+    
 }
 
